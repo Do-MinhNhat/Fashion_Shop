a9bd21318b6b20bc1be291cf13ad35c9f8ec548e
Do-MinhNhat <dominhnhat328@gmail.com>
2026-01-02 10:32:14 +0700

    Khởi tạo Model và Controller và các Policy, Requests

---
+++ app/Http/Controllers/ImportDetailController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ImportDetail;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreImportDetailRequest;
+use App\Http\Requests\UpdateImportDetailRequest;
+
+class ImportDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreImportDetailRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ImportDetail $importDetail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ImportDetail $importDetail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateImportDetailRequest $request, ImportDetail $importDetail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ImportDetail $importDetail)
+    {
+        //
+    }
+}
