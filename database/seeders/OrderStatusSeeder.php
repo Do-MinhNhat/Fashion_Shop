0d282b058939d460d2304406de07f2b52456b099
Manhcuong-cmd <truongmanhcuong2017@gmail.com>
2026-01-04 20:46:27 +0700

    fix seeder

--- database/seeders/OrderStatusSeeder.php
+++ database/seeders/OrderStatusSeeder.php
@@ -2,16 +2,25 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\OrderStatus;
 
 class OrderStatusSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $statuses = [
+            'Chờ xác nhận',
+            'Đã xác nhận',
+            'Đang xử lý',
+            'Hoàn thành',
+            'Đã hủy',
+        ];
+
+        foreach ($statuses as $name) {
+            OrderStatus::create([
+                'name' => $name,
+            ]);
+        }
     }
 }
--- database/seeders/SlideSeeder.php
+++ database/seeders/SlideSeeder.php
@@ -2,16 +2,33 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Slide;
 
 class SlideSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $slides = [
+            [
+                'image' => 'slide-1.jpg',
+                'url' => '/products',
+                'status' => 1,
+            ],
+            [
+                'image' => 'slide-2.jpg',
+                'url' => '/category/tee',
+                'status' => 1,
+            ],
+            [
+                'image' => 'slide-3.jpg',
+                'url' => '/category/shoes',
+                'status' => 1,
+            ],
+        ];
+
+        foreach ($slides as $slide) {
+            Slide::create($slide);
+        }
     }
 }
