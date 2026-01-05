8f07f1f5a0b2376b4914a10a625adc54f4569d64
Do-MinhNhat <dominhnhat328@gmail.com>
2026-01-05 22:51:39 +0700

    Chỉnh sửa gấp migration, yêu cầu xóa database và chạy lại!

--- database/migrations/2026_01_02_032732_create_orders_table.php
+++ database/migrations/2026_01_02_032732_create_orders_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(User::class)->nullable();
+            $table->foreignIdFor(User::class)->nullable()->constrained();
             $table->string('name');
             $table->string('phone')->index();
             $table->string('address');
