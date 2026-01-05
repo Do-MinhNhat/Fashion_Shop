05ef68b140494a94293791d32bd1db1849901040
Do-MinhNhat <dominhnhat328@gmail.com>
2026-01-05 17:19:36 +0700

    Sửa branch

--- database/seeders/TagSeeder.php
+++ database/seeders/TagSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TagSeeder extends Seeder
 {
